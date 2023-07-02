1a2cd62e5f9a6456e13c92058eeb86c9a44eb744
kadoum amine <kadoumamine@gmail.com>
2023-07-02 22:28:55 +0100

    committed

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Global from "./components/global";
 import Inthefutur from "./components/inthefutur";
 
-
 function App() {
-
-
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Global/>} />
-          <Route path="/soon" element={<Inthefutur/>} />
+          <Route path="/" element={<Global />} />
+          <Route path="/soon" element={<Inthefutur />} />
         </Routes>
       </Router>
     </>
