d8ec6cd71913bd238fe28a06468527cdc6f65e48
kadoum amine <kadoumamine@gmail.com>
2023-07-02 22:21:59 +0100

    new commit

--- vite-project/src/components/landingpage.jsx
+++ vite-project/src/components/landingpage.jsx
@@ -12,7 +12,7 @@ export default function Landingpage() {
         <section className=" sectionblur"></section>
         <div className=" flex flex-col place-content-center pl-20">
           <div className=" w-max div1 px-5 py-2 rounded-2xl shadow-lg mb-8 ">
-            <p className=" text-lg uppercase">
+            <p className="  text-lg uppercase">
               {" "}
               <FontAwesomeIcon
                 icon={faPercent}
